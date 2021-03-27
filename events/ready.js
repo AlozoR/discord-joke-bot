366473c26550e5e5a8158716bca4881d27aa4b89
alozor <nicolas-mery@neuf.fr>
2021-03-27 22:14:11 +0100

    chore: language translation

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,7 @@
 module.exports = (client) => {
-  console.log(`Je suis connecté et prêt à servir `+client.guilds.cache.size +" serveurs et "+client.users.cache.size+" utilisateurs");
+  console.log('Connected and ready to work for ' +
+    client.guilds.cache.size +
+    " servers and " +
+    client.users.cache.size +
+    " users");
 }
